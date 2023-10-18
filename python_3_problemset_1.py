57cc5741f697d7ea6ee72fd285c26289b19fb568
pfb23 <pfb23@info23.cshl.edu>
2023-10-18 12:43:29 -0400

    more files

---
+++ python_3_problemset_1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+DNA='ATTGGGCCCC'
+count_A=DNA.count('A')
+print(f'A= {count_A}')
+count_T=DNA.count('T')
+print(f'T= {count_T}')
+count_C=DNA.count('C')
+print(f'C= {count_C}')
+count_G=DNA.count('G')
+print(f'G= {count_G}')
+
+
+
